01a4e63b86dc54562a30db912c110e033f3a5091
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-05-23 14:07:58 +0200

    add: server.php

---
+++ server.php
@@ -0,0 +1,17 @@
+<?php
+
+$movies_string = file_get_contents('dischi.json'); // prende il file ma è string
+
+$movies_info = json_decode($movies_string, true); // trasforma la stringa in un array di array
+
+$data = [
+    'results' => $movies_info
+];
+
+$response = json_encode($data); // trasformo di nuovo l'array in json string
+
+
+
+header("Content-Type: application/json"); // dico al browser che sto inviando un file json
+
+echo $response; //invio la risposta
