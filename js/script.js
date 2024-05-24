d8244a65d5e809324ac197797ed72f50e9dbe8e6
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-05-24 17:56:21 +0200

    add: http request with GET method

--- js/script.js
+++ js/script.js
@@ -10,5 +10,17 @@ createApp({
         axios.get('http://localhost:8888/boolean/php-dischi-json/server.php').then((resp) => {
             this.disks = resp.data.results;
         });
+    },
+    methods: {
+        like(index) {
+            console.log(index);
+            axios.post("http://localhost:8888/boolean/php-dischi-json/server.php", {
+                params: {
+                    index
+                }
+            }).then((resp) => {
+                console.log(resp);
+            })
+        }
     }
 }).mount("#main");
--- server.php
+++ server.php
@@ -1,8 +1,16 @@
-<?php
+<?php 
 
 $disks_string = file_get_contents('dischi.json'); // prende il file ma è string
 
 $disks_info = json_decode($disks_string, true); // trasforma la stringa in un array di array
+
+if(isset($_POST['index'])) {
+
+    $like= $_POST['index'];
+
+    $disks_info;
+
+}
 
 $data = ['results' => $disks_info ];
 
